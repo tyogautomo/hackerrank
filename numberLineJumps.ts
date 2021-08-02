29f1810c1acfb180c67a0f2b620592b6c5dec97b
tyogautomo <yoga.utomo27@gmail.com>
2021-08-02 23:58:50 +0700

    ts file

--- numberLineJumps.ts
+++ numberLineJumps.ts
@@ -1,4 +1,4 @@
-const kangoroo = (x1, v1, x2, v2) => {
+const kangoroo = (x1: number, v1: number, x2: number, v2: number) => {
 
   const constraintX = x1 < x2;
   const constraintV = (v1 >= 1) && (v1 <= 10000) && (v2 >= 1) && (v2 <= 10000);
@@ -13,7 +13,7 @@ const kangoroo = (x1, v1, x2, v2) => {
       if (x1 > x2) return 'NO';
       start1 += v1;
       start2 += v2;
-      await sleep(1000);
+      // sleep(1000);
     }
     return 'YES';
   } else {
@@ -21,13 +21,14 @@ const kangoroo = (x1, v1, x2, v2) => {
   }
 };
 
-function sleep(time) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, time);
-  });
-}
+function sleep(milliseconds: number) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
+};
 
 console.log(kangoroo(0, 3, 4, 2));
 // console.log(kangoroo(0, 2, 5, 3));
